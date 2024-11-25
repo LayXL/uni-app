c7b66a67e7d8a8dad5127b7a70a49c3ecc8d8b8d
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-25 12:40:52 +0500

    impr

--- apps/web/src/features/map-view/ui/map-view.tsx
+++ apps/web/src/features/map-view/ui/map-view.tsx
@@ -1,20 +1,5 @@
 "use client"
 
-import { MapGestures } from "@/features/map-view/ui/map-gestures"
-
 export const MapView = () => {
-  return (
-    <MapGestures />
-    // <svg
-    //   width="581"
-    //   height="401"
-    //   viewBox="0 0 581 401"
-    //   fill="none"
-    //   xmlns="http://www.w3.org/2000/svg"
-    // >
-    //   <title>123</title>
-    //   <rect width="581" height="401" fill="black" />
-    //   <rect x="80" y="103" width="153" height="176" fill="#D9D9D9" />
-    // </svg>
-  )
+  return null
 }
