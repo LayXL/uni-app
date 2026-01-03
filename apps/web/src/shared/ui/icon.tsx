c2702ff7f762394b8af66e639514313237a99125
layxl <unstablexl@gmail.com>
2026-01-03 23:36:50 +0500

    icons

---
+++ apps/web/src/shared/ui/icon.tsx
@@ -0,0 +1,31 @@
+import type { ClassValue } from "clsx"
+
+import { cn } from "../utils/cn"
+
+export type IconProps = {
+	name: string
+	size?: number
+	color?: string
+	className?: ClassValue
+}
+
+export const Icon = (props: IconProps) => {
+	const size = props.size ?? Number(props.name.slice(-2))
+
+	return (
+		<span
+			className={cn(
+				"icon-mask block size-(--size) bg-current",
+				props.className,
+			)}
+			style={{
+				"--size": `${size}px`,
+				WebkitMaskImage: `url(/icons/${props.name}.svg)`,
+				maskImage: `url(/icons/${props.name}.svg)`,
+				WebkitMaskSize: "var(--size)",
+				maskSize: "var(--size)",
+				color: props.color ?? "currentcolor",
+			}}
+		/>
+	)
+}
