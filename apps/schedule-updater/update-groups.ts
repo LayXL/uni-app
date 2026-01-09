7ca997b669f4d1832dadd4cb16d9a57c02227774
layxl <unstablexl@gmail.com>
2026-01-09 14:30:31 +0500

    Remove update-groups.ts file as it is no longer needed for group updates in the schedule-updater application.

--- apps/schedule-updater/update-groups.ts
+++
@@ -1,9 +0,0 @@
-import { getAllGroups } from "@repo/bitrix/schedule/get-all-groups"
-import { getSession } from "@repo/bitrix/session/get-session"
-import { env } from "@repo/env"
-
-const { cookie } = await getSession(env.bitrixLogin, env.bitrixPassword)
-
-await getAllGroups(cookie)
-
-process.exit(0)
