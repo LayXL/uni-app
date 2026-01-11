468c47c3416502e1142c4ce3326f9e9be2c9589f
layxl <unstablexl@gmail.com>
2026-01-11 21:01:16 +0500

    Integrate debugging functionality into TmaProvider by adding setDebug call during SDK initialization to facilitate troubleshooting.

--- apps/web/src/app/_providers/tma-provider.tsx
+++ apps/web/src/app/_providers/tma-provider.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { init } from "@tma.js/sdk-react"
+import { init, setDebug } from "@tma.js/sdk-react"
 import { type ReactNode, useEffect } from "react"
 
 export const TmaProvider = ({ children }: { children: ReactNode }) => {
 	useEffect(() => {
 		try {
 			init()
+			setDebug(true)
 		} catch (error) {
 			// biome-ignore lint/suspicious/noConsole: tma sdk error
 			console.log("Failed to initialize TMA SDK", error)
