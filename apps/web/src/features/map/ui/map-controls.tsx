494a9cc56b24e45507a3e0d4c53dca8aad1c8a27
layxl <unstablexl@gmail.com>
2026-01-11 12:18:57 +0500

    Refactor MapControls to improve layout and responsiveness; adjust transform values for vertical positioning and opacity, and streamline pointer events handling for better user interaction.

--- apps/web/src/features/map/ui/map-controls.tsx
+++ apps/web/src/features/map/ui/map-controls.tsx
@@ -27,32 +27,29 @@ export const MapControls = ({
 
 	const value = useTransform(scrollY, [0, heightSafe], [0, 1])
 
-	const y = useTransform(value, [0, 0.7], ["-50%", "-200%"])
+	const y = useTransform(value, [0, 0.6], ["-50vh", "-100vh"])
 	const opacity = useTransform(value, [0.3, 0.4], [1, 0])
 	const pointerEvents = useTransform(value, [0.3, 0.38], ["auto", "none"])
 
 	return (
-		<>
-			<motion.div
-				className="absolute top-1/2 left-3"
-				style={{ y, opacity, pointerEvents }}
-			>
+		<motion.div
+			className="absolute w-full px-3 flex justify-between items-center pointer-events-none"
+			style={{ y, opacity }}
+		>
+			<motion.div style={{ pointerEvents }}>
 				<FloorControls
 					activeFloor={activeFloor}
 					onChangeFloor={onChangeFloor}
 				/>
 			</motion.div>
 
-			<motion.div
-				className="absolute top-1/2 right-3"
-				style={{ y, opacity, pointerEvents }}
-			>
+			<motion.div style={{ pointerEvents }}>
 				<PositionControls
 					zoomByStep={zoomByStep}
 					rotation={rotation}
 					resetRotation={resetRotation}
 				/>
 			</motion.div>
-		</>
+		</motion.div>
 	)
 }
