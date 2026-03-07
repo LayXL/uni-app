c8f8c95b6ce243176b18aafac69ce0d722d2b635
layxl <unstablexl@gmail.com>
2026-03-07 23:17:07 +0300

    Update Next.js configuration and redefine events table schema
    - Changed reactStrictMode from true to false in the Next.js configuration for potential performance improvements during development.
    - Replaced the existing events table schema with a new CREATE TABLE statement, defining the structure and constraints for the events table, including new fields for better event management.

--- packages/drizzle/migrations/0015_deep_nighthawk.sql
+++ packages/drizzle/migrations/0015_deep_nighthawk.sql
@@ -1,7 +1,14 @@
-ALTER TABLE "events" ADD COLUMN "title" varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "description" text;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "coverImage" text;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "groupsRegex" text;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "date" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "buttonUrl" text;--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "buttonText" varchar(255);
+CREATE TABLE "events" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"author" integer,
+	"title" varchar(255) NOT NULL,
+	"description" text,
+	"coverImage" text,
+	"groupsRegex" text,
+	"date" timestamp NOT NULL,
+	"buttonUrl" text,
+	"buttonText" varchar(255)
+);
+--> statement-breakpoint
+ALTER TABLE "events" ADD CONSTRAINT "events_author_users_id_fk" FOREIGN KEY ("author") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
