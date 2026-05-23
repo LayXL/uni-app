0bfd50da616afa1b6b41d76583bc1bddb2921bb9
layxl <unstablexl@gmail.com>
2026-05-24 00:36:04 +0300

    Enhance DebugResetUserGroupButton functionality
    - Updated the button to conditionally render based on user admin status and environment, improving access control.
    - Added a router redirect to the onboarding page after resetting the user group, enhancing user flow.

--- apps/web/src/app/(morda)/settings/_ui/debug-reset-user-group-button.tsx
+++ apps/web/src/app/(morda)/settings/_ui/debug-reset-user-group-button.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useQueryClient } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
 
 import { orpc } from "@repo/orpc/react"
 
@@ -10,16 +11,20 @@ import { Button } from "@/shared/ui/button"
 export const DebugResetUserGroupButton = () => {
 	const user = useUser()
 	const queryClient = useQueryClient()
+	const router = useRouter()
 
-	if (user.isAdmin) return null
+	if (!user.isAdmin && process.env.NODE_ENV !== "development") return null
 
 	return (
 		<Button
+			leftIcon="refresh-24"
 			variant="secondary"
 			onClick={async () => {
 				await orpc.users.updateUserGroup.call({ groupId: null })
 
 				queryClient.invalidateQueries({ queryKey: orpc.users.me.queryKey() })
+
+				router.replace("/onboarding")
 			}}
 			label="Сбросить группу"
 		/>
